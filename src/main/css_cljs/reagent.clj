f31a5bc3fc91bfa066b7de39d4fab104cff0df10
Karol Wójcik <karol.wojcik@tuta.io>
2020-07-23 14:22:02 +0200

    Don't fully qualify React components
    Otherwise incorrect class names are generated

--- src/main/css_cljs/reagent.clj
+++ src/main/css_cljs/reagent.clj
@@ -2,10 +2,9 @@
 
 (defmacro defstyled
   [aname body]
-  (let [ns-name# (str (get-in &env [:ns :name]))
-        full-name-inner (str ns-name# "/" aname)
+  (let [full-name-inner# (str aname)
         afn# (first body)
         component# (second body)
-        full-name (str ns-name# "/" (str component#))]
-    `(def ~aname (~afn# (with-meta ~component# {:display-name-inner ~full-name-inner
-                                                :display-name ~full-name})))))
+        full-name# (str component#)]
+    `(def ~aname (~afn# (with-meta ~component# {:display-name-inner ~full-name-inner#
+                                                :display-name ~full-name#})))))
